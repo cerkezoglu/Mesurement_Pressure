00485049918e4d2c7dbae1736cf47f7ceb9aa16b
cerkezoglu <abdullahcerkezoglu@gmail.com>
2020-12-12 04:09:56 +0300

    nothing really changed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-a  = pd.read_table('110170109.txt', sep='\s+')
+a  = pd.read_table('dataset3.txt', sep='\s+')
 columns = ['Time', '(s)', 'Voltage', '(V)']
 
 df = pd.DataFrame(a.to_numpy().reshape(-1,len(columns)), columns=columns)
@@ -12,5 +12,6 @@ df = pd.DataFrame(a.to_numpy().reshape(-1,len(columns)), columns=columns)
 time = df['(s)']
 voltage = df['Voltage']
 Table = np.concatenate((time.T, voltage.T)).reshape((2, -1))
-Table[1]= np.round_(Table[1], 6)
-pd.DataFrame(Table.T).to_csv("fixed_data.csv")
+Table[1] = np.round_(Table[1], 6)
+Table[0] = np.round_(Table[0], 3)
+pd.DataFrame(Table.T).to_csv("dataset3.csv", header= ["Time", "Voltage"])
